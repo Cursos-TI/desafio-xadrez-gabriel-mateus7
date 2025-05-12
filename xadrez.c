f4cf6ae66064d2da8637eabb7e98e07a209e36c8
gabriel-mateus7 <gabrielmateus436@gmail.com>
2025-05-12 17:25:03 +0000

    Realizacao nivel aventureiro no desafio xadrez

--- xadrez.c
+++ xadrez.c
@@ -3,8 +3,11 @@ int main (){
     //Movimento da torre 5 casas para direita
     int torre = 1;
     printf("----TORRE----\n\n");
+
+    printf("Movimento torre: \n");
+
     while(torre <=5 ){
-        printf("Movimento torre: Uma casa  para direita.\n");
+        printf("Direita.\n");
         torre++;
     }
 
@@ -15,25 +18,53 @@ int main (){
      printf("----BISPO----\n");
     int bispo = 1;
 
+    printf("Movimento Bispo:\n");
+
     for (bispo = 1; bispo < 6; bispo++)
     {
-       printf("Movimento Bispo: Uma casa direita.\n");
-       printf("Movimento Bispo: Para casa para cima.\n");
+       printf(" Direita.\n");
+       printf(" Cima.\n");
     }
 
     printf("-------------------------------------------\n");
     
     // movimento rainha 5 casas para esquerda
 
+
      printf("----RAINHA----\n");
     int rainha =1;
+
+    printf("Movimento rainha:\n");
      do
      {
-        printf("Movimento rainha: Uma casa  para esquerda.\n");
+        printf(" Esquerda.\n");
         rainha++;
      } while (rainha <= 8);
+
+
+       printf("-------------------------------------------\n");
+
+     
+// movimento CAVALO 2 casas para BAIXO E UMA PARA ESQUERDA
+
+     printf("----CAVALO----\n");
+    int cavaloBaixo = 1 ;
+    int cavaloEsquerda = 1;
+
+
+     printf("Movimento Cavalo:\n");
+
+    for (cavaloEsquerda = 1; cavaloEsquerda <= 1; cavaloEsquerda++)
+    {
+        while (cavaloBaixo <=2)
+        {
+             printf("Baixo.\n");
+             cavaloBaixo++;
+        }
+        printf("Esquerda.\n");
+        
+    }
+      printf("-------------------------------------------\n");
     
-
-
     return 0;
 }
